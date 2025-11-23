ca289a7eb1d4698fa120b96965d312e67cb13cc8
Mwendia1 <elijah.mwendia@student.moringaschool.com>
2025-11-23 21:11:00 +0300

    add order.py changes

---
+++ lib/order.py
@@ -0,0 +1,42 @@
+class Order:
+    all = []
+
+    def __init__(self, customer, coffee, price):
+        self.customer = customer
+        self.coffee = coffee
+        self.price = price
+        Order.all.append(self)
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @customer.setter
+    def customer(self, value):
+        from customer import Customer
+        if not isinstance(value, Customer):
+            raise Exception("Order customer must be a Customer instance.")
+        self._customer = value
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @coffee.setter
+    def coffee(self, value):
+        from coffee import Coffee
+        if not isinstance(value, Coffee):
+            raise Exception("Order coffee must be a Coffee instance.")
+        self._coffee = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, (int, float)):
+            raise Exception("Price must be a number.")
+        if not (1.0 <= value <= 10.0):
+            raise Exception("Price must be between 1.0 and 10.0.")
+        self._price = float(value)
